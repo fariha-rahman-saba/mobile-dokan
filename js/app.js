fb940276857e251fcf3885c749553c04d5a799bd
saba rahman <sabarahman.official@gmail.com>
2022-03-02 00:23:27 +0600

    more details  added

--- js/app.js
+++ js/app.js
@@ -62,21 +62,30 @@ const phoneDetails = (id) => {
     fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
         .then(res => res.json())
         .then(data => {
-            const selectedPhone = data.data;
-            console.log(selectedPhone);
+            // const selectedPhone = data.data;
+            // console.log(selectedPhone);
+
+            // const sensors = data.data.sensors;
 
             const div = document.createElement("div");
             detailView.innerHTML = "";
             div.innerHTML = `
-                <div class="card phone-detail" style="width: 36rem;">
-                    <img src="${selectedPhone.image}" class="card-img-top phone-detail" alt="...">
+                <div class="card phone-detail-section text-center" style="width: 36rem;">
+                    <img src="${data.data.image}" class="card-img-top phone-detail-img" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">${selectedPhone.releaseDate}</h5>
-                        <p class="card-text">${selectedPhone.mainFeatures}</p>
-                        <p class="card-text">${selectedPhone.sensors}</p>
-                        <p class="card-text">${selectedPhone.others}</p>
+                        <h5 class="card-title">Release Date: ${data.data.releaseDate}</h5>
+                        <p class="card-text">Storage: ${data.data.mainFeatures.storage}</p>
+                        <p class="card-text">Display: ${data.data.mainFeatures.displaySize}</p>
+                        <p class="card-text">Memory: ${data.data.mainFeatures.memory}</p>
+                        
+                        <p class="card-text">Sensors: ${data.data.sensors}</p>
+
+                        <p class="card-text">NFC: ${data.data.others.NFC}</p>
+                        <p class="card-text">${data.data.others.Radio}</p>
+                        <p class="card-text">${data.data.others.USB}</p>
                     </div>
                 </div>
+                
             `
             detailView.appendChild(div);
 
